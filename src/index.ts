71577a6c5b6073a3be53513f093e64e69f184cd4
hayabusabusa <hayabusabusaSY@gmail.com>
2022-06-30 20:45:05 +0900

    feat: 確認用のテストコードを追加

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-console.log("Hello");
+import { Client } from "discord.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const client = new Client({
+  intents: ["GUILDS", "GUILD_MESSAGES"],
+});
+
+client.once("ready", () => {
+  console.log("Ready");
+});
+
+client.on("message", (message) => {
+  // BOT からのメッセージは無視する.
+  if (message.author.bot) {
+    return;
+  }
+
+  if (message.content === "テスト") {
+    message.channel.send("こんにちは");
+  }
+});
+
+client.login(process.env.TOKEN);
