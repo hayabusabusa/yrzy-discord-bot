d4e2bdf178514e9d252715671fa1259beee7c3be
hayabusabusa <hayabusabusaSY@gmail.com>
2022-11-20 21:15:28 +0900

    fix: コマンド登録、実行時の処理を置き換え

--- src/index.ts
+++ src/index.ts
@@ -2,69 +2,40 @@ import { exec } from "child_process";
 import { Client } from "discord.js";
 import dotenv from "dotenv";
 
+import { Command, ServerCommand } from "./command";
+
 dotenv.config();
 
 const client = new Client({
   intents: ["GUILDS", "GUILD_MESSAGES"],
 });
 
+const commands = new Map<string, Command>([
+  ["server", new ServerCommand()],
+]);
+
 // BOT 起動時の動作
 client.once("ready", async () => {
+  // 登録するコマンドの準備
+  const resolvedCommands = Array.from(commands.values())
+    .map((element) => { return element.resolved() });
+    
   // コマンドの登録
-  await client.application?.commands.set([
-    {
-      name: "server",
-      description: "Start or stop game server",
-      options: [
-        {
-          type: "SUB_COMMAND",
-          name: "start",
-          description: "Start server",
-          options: [
-            {
-              type: "STRING",
-              name: "game",
-              description: "Select game title",
-              required: true,
-              choices: [
-                {
-                  name: "Terraria",
-                  value: "terraria"
-                },
-                {
-                  name: "Minecraft",
-                  value: "minecraft"
-                }
-              ]
-            }
-          ]
-        },
-        {
-          type: "SUB_COMMAND",
-          name: "stop",
-          description: "Stop server",
-          options: [
-            {
-              type: "STRING",
-              name: "game",
-              description: "Select game title",
-              required: true,
-              choices: [
-                {
-                  name: "Terraria",
-                  value: "terraria"
-                },
-                {
-                  name: "Minecraft",
-                  value: "minecraft"
-                }
-              ]
-            }
-          ]
-        },
-      ]
-    }
-  ], process.env.SERVER_ID ?? "");
+  await client.application?.commands.set(resolvedCommands, process.env.SERVER_ID ?? "");
+});
+
+// `/` コマンドが実行された時の動作
+client.on("interactionCreate", async (interaction) => {
+  // コマンド以外は無視する
+  if (!interaction.isCommand()) return;
+
+  // 定義ずみのコマンドかどうかを確認.
+  const command = commands.get(interaction.commandName);
+  if (command == null) {
+    return;
+  }
+
+  await command.execute(interaction);
 });
 
 // メンション付きのメッセージが送信された時の動作
@@ -78,102 +49,5 @@ client.once("ready", async () => {
 //     await message.channel.send(`${message.author.toString()} メッセージを送信しましたー`);
 //   }
 // });
-
-const execPromise = async (command: string) => {
-  return new Promise<string>((resolve, reject) => {
-    exec(command, (error, stdout, stderr) => {
-      if (error != null) reject(error);
-
-      resolve(stdout);
-    })
-  })
-}
-
-const isRunning = async (game: string) => {
-  const stdout = await execPromise("gcloud compute instances list");
-  // インスタンスの状態をログから取り出し
-  const splited = stdout.split(/\r\n|\n/)
-  return splited.find((value) => value.includes(game))?.includes("RUNNING") ?? true;
-}
-
-// `/` コマンドが実行された時の動作
-client.on("interactionCreate", async (interaction) => {
-  // コマンド以外は無視する
-  if (!interaction.isCommand()) return;
-
-  if (interaction.commandName === "server") {
-    // コマンドのオプションからゲームのタイトルを取り出し
-    const game = interaction.options.getString("game");
-
-    if (game == null) {
-      await interaction.reply({
-        content: "ゲーム名を教えてねー",
-      });
-      return;
-    }
-
-    // インスタンスの立ち上げには時間がかかるので `defer` 指定にしておく
-    await interaction.deferReply();
-
-    switch (interaction.options.getSubcommand()) {
-      case "start":
-        const isServerRunningForStart = await isRunning(game);
-
-        if (isServerRunningForStart) {
-          await interaction.editReply("すでにサーバーは起動されているっぽいよ？？");
-          return;
-        }
-
-        try {
-          const _ = await execPromise(`gcloud --account ${process.env.GCP_SERVICE_ACCOUNT_ID} compute instances start ${game + process.env.GCP_SERVER_INSTANCE_NAME_SUFFIX} --zone ${process.env.GCP_SERVER_INSTANCE_ZONE}`);
-        } catch (error) {
-          await interaction.editReply({
-            content: `サーバー起動時にエラーが発生したみたい...`,
-            embeds: [
-              {
-                color: 0xf44336,
-                description: `${error}`
-              }
-            ]
-          });
-          return;
-        }
-        
-        await interaction.editReply(`${game} のサーバーを起動したよー`);
-
-        break;
-      case "stop":
-        const isServerRunningForStop = await isRunning(game);
-
-        if (!isServerRunningForStop) {
-          await interaction.editReply("すでにサーバーは停止されているっぽいよ？？");
-          return;
-        }
-
-        try {
-          const _ = await execPromise(`gcloud --account ${process.env.GCP_SERVICE_ACCOUNT_ID} compute instances stop ${game + process.env.GCP_SERVER_INSTANCE_NAME_SUFFIX} --zone ${process.env.GCP_SERVER_INSTANCE_ZONE}`);
-        } catch (error) {
-          await interaction.editReply({
-            content: `サーバー起動時にエラーが発生したみたい...`,
-            embeds: [
-              {
-                color: 0xf44336,
-                description: `${error}`
-              }
-            ]
-          });
-          return;
-        }
-
-        await interaction.editReply(`${game} のサーバーを停止したよー`);
-
-        break;
-      default:
-        await interaction.editReply("コマンドに間違いがあるかも💦");
-
-        break;
-    }
-  }
-});
 
 client.login(process.env.TOKEN);
